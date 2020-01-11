1ae03441470efdc4235f77b60a0b3e34714e209a
Samir Georgy <57589276+samirgeorgy@users.noreply.github.com>
2020-01-12 00:44:49 +0400

    Added Spawn Manager

---
+++ Space Shooter Pro/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    #region Private Variables
+
+    [SerializeField] private float _spawnWait = 5f;
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _enemyContainer;
+
+    #endregion
+
+    #region Unity Functions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Supporting Functions
+
+    /// <summary>
+    /// The enemy spawning routine
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            yield return new WaitForSeconds(_spawnWait);
+        }
+    }
+
+    #endregion
+}
