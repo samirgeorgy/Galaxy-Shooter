1a81dc3ecbcaa1d28e0f5b6f4ef0092b848873ff
Samir Georgy <57589276+samirgeorgy@users.noreply.github.com>
2020-01-11 18:55:57 +0400

    Created Player Script

---
+++ Space Shooter Pro/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Private Variables
+
+    [SerializeField] private float _speed = 3.5f;
+
+    #endregion
+
+    #region Unity Functions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    #endregion
+
+    #region Supporting Functions
+
+    /// <summary>
+    /// Moves the player
+    /// </summary>
+    private void CalculateMovement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalMovement, verticalMovement, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.3f)
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        else if (transform.position.x < -11.3f)
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+    }
+
+    #endregion
+}
